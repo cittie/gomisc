978c80b8a0b78dec93447419f88b637a97fa4e29
JiangYi <jiangyi@taiyouxi.cn>
2020-04-07 17:04:29 +0800

    Add method to get field name of struct recursively

---
+++ reflect.go
@@ -0,0 +1,60 @@
+package gomisc
+
+import (
+	"reflect"
+)
+
+const (
+	maxIterCount = 128
+)
+
+// GetFieldNamesRecursively Recursively return field names of an interface
+// if input is not a struct, return type name instead
+func GetFieldNamesRecursively(data interface{}) []string {
+	return getFieldNamesRecur(data, 0)
+}
+
+func getFieldNamesRecur(data interface{}, iterCount int) []string {
+	if data == nil {
+		return nil
+	}
+
+	// avoid infinite recursive
+	iterCount++
+	if iterCount > maxIterCount {
+		return nil
+	}
+
+	t := reflect.TypeOf(data)
+	v := reflect.ValueOf(data)
+
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	if t.Kind() != reflect.Struct {
+		return []string{t.Name()}
+	}
+
+	n := t.NumField()
+
+	fieldNames := make([]string, 0, n)
+
+	for i := 0; i < n; i++ {
+		switch t.Field(i).Type.Kind() {
+		case reflect.Struct:
+			if v.Field(i).CanAddr() {
+				fieldNames = append(fieldNames, GetFieldNamesRecursively(v.Field(i).Addr().Interface())...)
+			} else {
+				fieldNames = append(fieldNames, GetFieldNamesRecursively(v.Field(i).Interface())...)
+			}
+		default:
+			fieldNames = append(fieldNames, t.Field(i).Name)
+		}
+	}
+
+	return fieldNames
+}
---
+++ reflect_test.go
@@ -0,0 +1,61 @@
+package gomisc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type MainStruct struct {
+	MID string
+	SubStruct
+	mNum int
+}
+
+type SubStruct struct {
+	ID  string
+	num int
+}
+
+type CustomType int64
+
+func newTestStruct() *MainStruct {
+	ms := new(MainStruct)
+	ms.MID = "MainID"
+	ms.ID = "subID"
+	ms.num = 19
+	ms.mNum = 734
+
+	return ms
+}
+
+func TestGetFieldNamesRecur(t *testing.T) {
+	structVal := newTestStruct()
+	var intVal int64 = 32
+	var cusVal CustomType = 255
+
+	tests := []struct {
+		in  interface{}
+		out []string
+	}{
+		{structVal, []string{"MID", "ID", "num", "mNum"}},
+		{*structVal, []string{"MID", "ID", "num", "mNum"}},
+		{intVal, []string{"int64"}},
+		{&intVal, []string{"int64"}},
+		{cusVal, []string{"CustomType"}},
+		{&cusVal, []string{"CustomType"}},
+	}
+
+	for i, test := range tests {
+		assert.Equal(t, test.out, GetFieldNamesRecursively(test.in), i)
+	}
+}
+
+func BenchmarkGetFieldNamesRecur(b *testing.B) {
+	b.ReportAllocs()
+	r := newTestStruct()
+
+	for i := 0; i < b.N; i++ {
+		GetFieldNamesRecursively(r)
+	}
+}
